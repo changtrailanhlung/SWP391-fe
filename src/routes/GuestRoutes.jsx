c959725eaa4f1c6cdfeb33802c9d528ccf86c6a5
changtrailanhlung <55450357+changtrailanhlung@users.noreply.github.com>
2024-10-02 11:50:02 +0700

    Add Page

--- src/routes/GuestRoutes.jsx
+++ src/routes/GuestRoutes.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import GuestLayout from "../layouts/GuestLayout";
 import Home from "../pages/guest/Home";
-// import About from "../pages/guest/About";
-// import Contact from "../pages/guest/Contact";
+import About from "../pages/guest/About";
+import Contact from "../pages/guest/Contact";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 
 const GuestRoutes = () => {
@@ -12,8 +12,8 @@ const GuestRoutes = () => {
       <Routes>
         <Route element={<GuestLayout />}>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
       <ScrollToTopButton />
