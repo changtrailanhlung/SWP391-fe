42c0c410f150afe59eda36e8dad6ba74692970df
changtrailanhlung <lehongnguyen1999@gmail.com>
2024-10-09 23:00:18 +0700

    Edit Routes

--- src/pages/guest/Donates.jsx
+++ src/pages/guest/Donates.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Contact() {
+function Donates() {
   return (
     <div className="home-page">
       <main className="flex items-center justify-center h-screen">
-        <h1 className="text-4xl text-gray-700">Welcome to the Contact Page</h1>
+        <h1 className="text-4xl text-gray-700">Welcome to the Donates Page</h1>
       </main>
     </div>
   );
 }
 
-export default Contact;
+export default Donates;
--- src/pages/guest/Events.jsx
+++ src/pages/guest/Events.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Contact() {
+function Events() {
   return (
     <div className="home-page">
       <main className="flex items-center justify-center h-screen">
-        <h1 className="text-4xl text-gray-700">Welcome to the Contact Page</h1>
+        <h1 className="text-4xl text-gray-700">Welcome to the Events Page</h1>
       </main>
     </div>
   );
 }
 
-export default Contact;
+export default Events;
