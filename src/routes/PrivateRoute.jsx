5206726c4525cb34abdffe82c0689f648c08b8d4
Hyzmyn <hung2k3olaf@gmail.com>
2024-10-10 11:30:54 +0700

    fix admin

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,7 +6,7 @@ const PrivateRoute = ({ element, allowedRoles }) => {
   const { user } = useAuth(); // Assuming `user` contains user info including roles
 
   const isAllowed = user && user.roles && user.roles.some(role => allowedRoles.includes(role)); // Replace `user.role` with your logic
-  const isAuthorized = userRoles.some(role => allowedRoles.includes(role));
+  
 
   return isAllowed ? element : <Navigate to="/admin/login" replace />;
 };
