14abfbe68459275929fd6701e1aa8f9f7db8e204
changtrailanhlung <lehongnguyen1999@gmail.com>
2024-10-11 22:58:31 +0700

    Update UserAccounts.jsx

--- src/pages/admin/UserAccounts.jsx
+++ src/pages/admin/UserAccounts.jsx
@@ -1,6 +1,4 @@
-// src/pages/admin/UserAccounts.jsx
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   GridComponent,
   ColumnsDirective,
@@ -11,8 +9,8 @@ import {
   Edit,
   Toolbar,
   Sort,
-  Filter
-} from '@syncfusion/ej2-react-grids';
+  Filter,
+} from "@syncfusion/ej2-react-grids";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -26,17 +24,21 @@ const UserAccounts = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('/users');
+        const response = await axios.get("/users");
         const usersWithRoles = await Promise.all(
           response.data.map(async (user) => {
-            const roleResponse = await axios.get(`/userrole/role/${user.id}/roles`);
-            const rolesArray = roleResponse.data.map(role => role.roles).flat();
-            return { ...user, roles: rolesArray.join(', ') };
+            const roleResponse = await axios.get(
+              `/userrole/role/${user.id}/roles`
+            );
+            const rolesArray = roleResponse.data
+              .map((role) => role.roles)
+              .flat();
+            return { ...user, roles: rolesArray.join(", ") };
           })
         );
         setUsers(usersWithRoles);
       } catch (error) {
-        console.error('Lỗi khi tải danh sách người dùng:', error);
+        console.error("Lỗi khi tải danh sách người dùng:", error);
         if (error.response && error.response.status === 401) {
           toast.error("Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại.");
           navigate("/admin/login");
@@ -50,27 +52,59 @@ const UserAccounts = () => {
   }, [navigate]);
 
   const selectionSettings = { persistSelection: true };
-  const toolbarOptions = ['Delete'];
+  const toolbarOptions = ["Delete"];
   const editing = { allowDeleting: true, allowEditing: true };
 
   const userColumns = [
-    { field: 'username', headerText: t('username'), width: '120', textAlign: 'Center' },
-    { field: 'email', headerText: t('email'), width: '150', textAlign: 'Center' },
-    { field: 'phone', headerText: t('phone'), width: '120', textAlign: 'Center', template: (row) => row.phone || 'N/A' },
-    { field: 'password', headerText: t('password'), width: '120', textAlign: 'Center', template: () => '********' },
-    { field: 'location', headerText: t('location'), width: '120', textAlign: 'Center', template: (row) => row.location || 'N/A' },
-    { field: 'roles', headerText: t('roles'), width: '150', textAlign: 'Center' },
+    {
+      field: "username",
+      headerText: t("username"),
+      width: "120",
+      textAlign: "Center",
+    },
+    {
+      field: "email",
+      headerText: t("email"),
+      width: "150",
+      textAlign: "Center",
+    },
+    {
+      field: "phone",
+      headerText: t("phone"),
+      width: "120",
+      textAlign: "Center",
+      template: (row) => row.phone || "N/A",
+    },
+    {
+      field: "password",
+      headerText: t("password"),
+      width: "120",
+      textAlign: "Center",
+      template: () => "********",
+    },
+    {
+      field: "location",
+      headerText: t("location"),
+      width: "120",
+      textAlign: "Center",
+      template: (row) => row.location || "N/A",
+    },
+    {
+      field: "roles",
+      headerText: t("roles"),
+      width: "150",
+      textAlign: "Center",
+    },
   ];
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      
       <div className="mb-10">
-    <p className="text-lg text-gray-400">Page</p>
-    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
-      {t("userAccounts")}
-    </p>
-  </div>
+        <p className="text-lg text-gray-400">Page</p>
+        <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+          {t("userAccounts")}
+        </p>
+      </div>
       <GridComponent
         dataSource={users}
         allowPaging
