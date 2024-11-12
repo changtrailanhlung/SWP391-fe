1cb357e6dd22c18b90ead397bacb1cf56c85c22b
changtrailanhlung <lehongnguyen1999@gmail.com>
2024-11-12 20:40:14 +0700

    Edit Multi Language

--- src/pages/guest/Wallet.jsx
+++ src/pages/guest/Wallet.jsx
@@ -1,64 +1,63 @@
 import React, { useState } from "react";
 import { post } from "../../services/axiosClient";
+import { useTranslation } from "react-i18next";
 
 const Wallet = () => {
-  const [amount, setAmount] = useState(""); // Default amount
+  const { t } = useTranslation();
+  const [amount, setAmount] = useState("");
 
   const handleDonate = async () => {
     const fullName = localStorage.getItem("username");
-    const userId = localStorage.getItem("nameid"); // Retrieve userId from localStorage
+    const userId = localStorage.getItem("nameid");
 
     if (!fullName || !userId) {
-      console.error("User information is missing from storage.");
+      console.error(t("wallet.missingUserInfo"));
       return;
     }
 
     const numericAmount = amount ? Number(amount) : 0;
 
-    // Check if the entered amount is valid
     if (numericAmount <= 0) {
-      alert("Vui lòng nhập số tiền hợp lệ.");
+      alert(t("wallet.invalidAmount"));
       return;
     }
 
     const data = {
       fullName,
-      userId, // Add userId to the data object
+      userId,
       amount: numericAmount,
-      description: "", // Description is an empty string
+      description: "",
     };
 
     try {
       const response = await post("/donate/vnpay", data);
-      // console.log("Donation successful:", response);
-
-      // Assume response contains the payment URL
-      const paymentUrl = response; // Assign directly if response contains the URL
-      window.location.href = paymentUrl; // Redirect to the payment URL
+      const paymentUrl = response;
+      window.location.href = paymentUrl;
     } catch (error) {
-      console.error("Error during donation:", error);
+      console.error(t("wallet.donationError"), error);
     }
   };
 
   return (
     <div className="max-w-md mx-auto p-4 my-8 border rounded-lg shadow-lg bg-white">
-      <h1 className="text-2xl font-bold mb-4">Wallet</h1>
+      <h1 className="text-3xl font-bold text-center mb-6">
+        {t("wallet.title")}
+      </h1>
       <label className="block mb-2">
-        <span className="text-gray-700">Số tiền:</span>
         <input
           type="number"
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
-          min="0" // Ensure the amount is non-negative
+          min="0"
           className="mt-1 block w-full p-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-          placeholder="Nhập số tiền"
+          placeholder={t("wallet.amountPlaceholder")}
         />
       </label>
       <button
         onClick={handleDonate}
         className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200"
       >
-        Donate
+        {t("wallet.donateButton")}
       </button>
     </div>
   );
